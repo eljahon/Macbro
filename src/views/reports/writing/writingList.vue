cab815054d7ee77daac71c58fe8c57b7aa3f0277
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-10-19 13:33:28 +0500

    fix: change the word pokupatel into cashier

--- src/views/reports/writing/writingList.vue
+++ src/views/reports/writing/writingList.vue
@@ -75,7 +75,7 @@ export default {
           }
         },
         {
-          title: 'Покупатель',
+          title: 'Кассир',
           scopedSlots: {
             customRender: 'customer',
             filterDropdown: 'userDropdown',
