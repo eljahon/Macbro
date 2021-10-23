37f2ca4f9c53e2b11b633efc4bd51cfaea0d5d80
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-10-24 02:34:56 +0500

    fix: FML this is so disgusting to work in this project

--- src/views/reports/writing/writingdetalinypage.vue
+++ src/views/reports/writing/writingdetalinypage.vue
@@ -1,19 +1,21 @@
 <template>
   <div v-if="render" style="background-color: transparent; position: relative">
-    <a-spin style="z-index: 9999; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)" size="large" />
+    <a-spin
+      style="z-index: 9999; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)"
+      size="large"
+    />
   </div>
   <div v-else>
     <a-card>
       <div slot="title">
-        <back-router-name :router="router"/>
+        <back-router-name :router="router" />
       </div>
       <div slot="extra">
-        <dowlon-button-excel/>
+        <dowlon-button-excel />
       </div>
       <a-card class="card_user_id">
         <div slot="title">
-          <id-number :text="router.text" :number="list.number"/>
-
+          <id-number :text="router.text" :number="list.number" />
         </div>
         <div slot="extra">
           <div class="wrapper_calss">
@@ -21,9 +23,9 @@
               <span class="user_Full_name">{{ list.cashier.first_name }} {{ list.cashier.last_name }}</span>
               <span class="user_type" style="float: right">{{ dataUseType.userType[list.cashier.user_type] }}</span>
             </span>
-            <img class="image" :src="list.cashier.profile_image" alt="">
+            <img class="image" :src="list.cashier.profile_image" alt="" />
           </div>
-        <!--        <min-card-rigth class="mincard" :name="`${list.cashier.first_name}${}`" :type="list.cashier.user_type" :image="list.cashier.profile_image"/>-->
+          <!--        <min-card-rigth class="mincard" :name="`${list.cashier.first_name}${}`" :type="list.cashier.user_type" :image="list.cashier.profile_image"/>-->
         </div>
       </a-card>
       <a-table
@@ -40,45 +42,60 @@
           <span style="display: inline; border-radius: 50%">
             <img
               style="object-fit: cover; max-height: 40px"
-              :src="row.product_image === ''? image :row.product_image"
-              alt="imgId">
+              :src="row.product_image === '' ? image : row.product_image"
+              alt="imgId"
+            />
           </span>
           <span v-if="row.product_image.length" style="margin-left:10px;">{{ row.product_name }}</span>
           <span v-else style="margin-left:5px; position: relative;">{{ row.product_name }}</span>
         </template>
         <template slot="Состояние" slot-scope="text, row">
-          <table-user-column :image="row.causer.profile_image" :name="`${row.causer.first_name} ${row.causer.last_name}`" :phone="row.causer.phone_number" :type="row.causer.user_type" />
+          <table-user-column
+            :image="row.causer.profile_image"
+            :name="`${row.causer.first_name} ${row.causer.last_name}`"
+            :phone="row.causer.phone_number"
+            :type="row.causer.user_type"
+          />
         </template>
         <template slot="Сумма" slot-scope="text, row">
-          <span v-if="row">{{ new Intl.NumberFormat('en-En', { style: 'currency', currency: 'USD' }).format(row.cash)
+          <span v-if="row">{{
+            new Intl.NumberFormat('en-En', { style: 'currency', currency: 'USD' }).format(row.cash)
           }}</span>
         </template>
         <template slot="Статус" slot-scope="text, row">
-          <a-tag :color="row.status === 'active' ? 'blue' : 'red'">{{ row.status === 'active' ? 'Активный' : 'Не активный' }}</a-tag>
+          <a-tag :color="row.status === 'active' ? 'blue' : 'red'">{{
+            row.status === 'active' ? 'Активный' : 'Не активный'
+          }}</a-tag>
 
-        <!--        <span v-if="row.status === 'in-process'"><a-tag :color="'rgba(24, 144, 255, 0.1)'">Принято</a-tag></span>-->
+          <!--        <span v-if="row.status === 'in-process'"><a-tag :color="'rgba(24, 144, 255, 0.1)'">Принято</a-tag></span>-->
         </template>
         <template slot="kolvo" slot-scope="text, row">
           <span v-if="row">{{ '1' }}</span>
           <!--        <a-tag :color="row.status === 'active' ? 'blue' : 'red'">{{ row.status === 'active' ? 'Активный' : 'Не активный' }}</a-tag>-->
 
-        <!--        <span v-if="row.status === 'in-process'"><a-tag :color="'rgba(24, 144, 255, 0.1)'">Принято</a-tag></span>-->
+          <!--        <span v-if="row.status === 'in-process'"><a-tag :color="'rgba(24, 144, 255, 0.1)'">Принято</a-tag></span>-->
         </template>
       </a-table>
-      <br>
+      <br />
     </a-card>
-    <br>
+    <br />
 
     <a-card class="car_commit">
       <div style="margin-top: 10px" slot="title" class="commit"><h2>Комментарии</h2></div>
     </a-card>
     <a-card class="car_commit_one">
-      <p class="comit_text">{{list.comment.length > 0? list.comment : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque commodi culpa cum cumque dolore est eum explicabo hic, nostrum possimus quam quasi quisquam repellendus, repudiandae sequi suscipit veritatis voluptatum?'}}</p>
-      <br>
-      <div v-for="(item,index) in (list.comment_images)" :key="index">
- <div style="display:flex; gap: 20px">
-   <div><img :src="item" alt=""></div>
- </div>
+      <p class="comit_text">
+        {{
+          list.comment.length > 0
+            ? list.comment
+            : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque commodi culpa cum cumque dolore est eum explicabo hic, nostrum possimus quam quasi quisquam repellendus, repudiandae sequi suscipit veritatis voluptatum?'
+        }}
+      </p>
+      <br />
+      <div v-for="(item, index) in list.comment_images" :key="index">
+        <div style="display:flex; gap: 20px">
+          <div><img :src="item" alt="" /></div>
+        </div>
       </div>
     </a-card>
   </div>
@@ -95,11 +112,13 @@ import TableUserColumn from '@/components/TableUserColumn/TableUserColumn.vue'
 import phone from '../../../assets/Rectangle11487.jpg'
 import image from '../../../assets/phone.svg'
 export default {
-  components: { IdNumber,
+  components: {
+    IdNumber,
     BackRouterName,
     'dowlon-button-excel': excelButton,
     'min-card-rigth': mincardRigith,
-TableUserColumn },
+    TableUserColumn
+  },
   data () {
     return {
       list: {},
@@ -164,8 +183,7 @@ TableUserColumn },
   methods: {
     ...mapActions(['getWritingListItemId']),
     getWritingListItem () {
-      this.getWritingListItemId(this.$route.params.id)
-      .then(res => {
+      this.getWritingListItemId(this.$route.params.id).then(res => {
         this.list = res
         this.router.userName = `${res.cashier.first_name}${' '}${res.cashier.last_name}`
         console.log(res)
@@ -183,17 +201,17 @@ TableUserColumn },
 </script>
 
 <style scoped>
-.section{
+.section {
   display: flex;
   justify-content: space-between;
 }
-.mincard{
+.mincard {
 }
 .card_user_id {
   border-left: none;
   border-right: none;
 }
-.image{
+.image {
   max-height: 40px;
   max-width: 40px;
   min-height: 40px;
@@ -205,10 +223,10 @@ TableUserColumn },
   border-radius: 50%;
   margin: 0px 16px;
 }
-.user_Full_name{
+.user_Full_name {
   white-space: nowrap;
   height: 22px;
-  font-family: Roboto,sans-serif;
+  font-family: Roboto, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 17px;
@@ -220,25 +238,25 @@ TableUserColumn },
   align-items: center;
   text-align: right;
 }
-.wrapper_calss{
+.wrapper_calss {
   display: flex;
-  align-items: flex-end
+  align-items: flex-end;
 }
-.user_type{
+.user_type {
   position: static;
   width: 141px;
   height: 16px;
   left: 0px;
   top: 24px;
-  font-family: Roboto,sans-serif;
+  font-family: Roboto, sans-serif;
   font-style: normal;
   font-weight: 500;
   font-size: 13px;
   line-height: 22px;
   text-align: right;
-  color: #818C99;
+  color: #818c99;
 }
-.user_name{
+.user_name {
   flex: 0 0 30%;
 }
 .commit {
@@ -285,12 +303,11 @@ TableUserColumn },
   height: 120px;
   left: 0px;
   top: 0px;
-  background: #F7F7FB;
+  background: #f7f7fb;
   border-radius: 8px;
   flex: none;
   order: 0;
   align-self: stretch;
   flex-grow: 0;
-
 }
 </style>
